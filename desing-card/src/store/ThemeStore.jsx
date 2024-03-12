3ae94c15638b4da21d31377580dfb63aae38b142
daryhen <daryhen@Proy.myguest.virtualbox.org>
2024-03-11 23:58:44 -0500

    theme with zustand

--- desing-card/src/store/ThemeStore.jsx
+++ desing-card/src/store/ThemeStore.jsx
@@ -1,7 +1,12 @@
 import { create } from 'zustand'
-import { Light } from './themes'
+import { Dark, Light } from './themes'
 
-const useThemeStore = create((set, get) => ({
+export const useThemeStore = create((set, get) => ({
         theme: 'light',
-        themeStyle: Light
+        themeStyle: Light,
+        setTheme: () => {
+            const {theme} = get()
+            set({theme: theme === 'light' ? 'dark' : 'light'})
+            set({themeStyle: theme === 'light' ? Dark : Light})
+        }
   }))
